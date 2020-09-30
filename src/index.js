927d2470c5224f68a9aa06d90db21cdf5c37bdbd
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-09-30 21:13:11 +0300

    refactor: add code acording task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+
+if (array === undefined || array[0] === undefined ) {
+        return 0}
+
+let newArr=array.sort(function (prev, next) {
+        return prev - next
+    });
+  return newArr[0];
 }
+
+
 
 exports.max = function max (array) {
-  return 0;
+
+if (array === undefined || array[0] === undefined ) {
+        return 0}
+
+let newArr=array.sort(function (prev, next) {
+        return prev - next
+    });   
+      return newArr[newArr.length-1];
 }
 
+
+
 exports.avg = function avg (array) {
-  return 0;
+
+if (array === undefined || array[0] === undefined ) {
+        return 0}
+
+let sum=0;
+    array.forEach(element => {sum=sum+element 
+  });
+    let aver=sum/array.length
+    return aver;
 }
